ed537d3e330fea31d6179733d2d6a693b8c2e749
yinqingchen_ <245691986@qq.com>
2026-03-17 19:30:05 +0800

    2

---
+++ event.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+  
+const count = ref(0)
+
+function increment()
+  {
+    count.value++
+  }
+</script>
+
+<template>
+  <!-- 使此按钮生效 -->
+  <!-- 1.监听按钮点击点击；2.调用一个加一的方法；3.把第二步的结果展示到按钮上-->
+  <button @click = "increment">Count is: {{ count }}</button>
+</template>
---
+++ if.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true)
+
+function toggle() {
+  // ...
+  awesome.value = !awesome.value
+}
+</script>
+
+<template>
+  <button @click="toggle">点击按钮代表爱yyl一天</button>
+  <h1 v-if = "awesome">天数 + 1</h1>
+  <h1 v-else>永远爱她</h1>
+</template>
